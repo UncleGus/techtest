92090b2a33651c49b120faa7069d05040d4de0d7
David Karl <david.karl@solnetsolutions.co.nz>
2021-03-18 13:24:02 +1300

    Initial commit for Westpace technical test

---
+++ cypress/integration/buggy/test.spec.ts
@@ -0,0 +1,64 @@
+context('Buggy site', () => {
+  context('Home page', () => {
+    beforeEach(() => {
+      cy.fixture('dashboard').as('dashboardData')
+      cy.intercept('dashboard', { fixture: 'dashboard' })
+      cy.visit('/')
+    })
+
+    it('Make votes are displayed correctly', function () {
+      cy.contains(this.dashboardData.make.name).find('small').should('have.text', `(${this.dashboardData.make.votes} votes)`)
+    })
+
+    it('Model votes are displayed correctly', function () {
+      cy.contains(`${this.dashboardData.model.make} ${this.dashboardData.model.name}`).find('small').should('have.text', `(${this.dashboardData.model.votes} votes)`)
+    })
+  })
+
+  const newUserName = makeValidPassword(8)
+  const newPassword = makeValidPassword(16)
+  context('Registration', () => {
+    before(() => {
+      cy.visit('/register')      
+    })
+
+    it('Can sign up a new user', () => {
+      cy.get('#username').clear().type(newUserName)
+      cy.get('#firstName').clear().type('Fake')
+      cy.get('#lastName').clear().type('Name')
+      cy.get('#password').clear().type(newPassword)
+      cy.get('#confirmPassword').clear().type(newPassword)
+
+      cy.contains('button', 'Register').click()
+
+      cy.contains('div.alert-success', 'Registration is successful')
+    })
+
+    it('Can log in with newly signed up user', () => {
+      cy.intercept('/prod/users/current').as('users')
+      cy.get('nav input[name="login"]').clear().type(newUserName)
+      cy.get('nav input[name="password"]').clear().type(newPassword)
+
+      cy.contains('button[type="submit"]', 'Login').click()
+      cy.wait('@users')
+
+      cy.contains('nav span', 'Hi, Fake')
+    })
+  })
+})
+
+function makeValidPassword(length) {
+  let result = ''
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  const numbers = '0123456789'
+  const symbols = `!@#$%^&*()_+[];',./{}:"<>?`
+  const lettersLength = letters.length
+  const numbersLength = numbers.length
+  const symbolsLength = symbols.length
+  for (let i = 0; i < length - 2; i++) {
+    result += letters.charAt(Math.floor(Math.random() * lettersLength))
+  }
+  result += numbers.charAt(Math.floor(Math.random() * numbersLength))
+  result += symbols.charAt(Math.floor(Math.random() * symbolsLength))
+  return result
+}
